13829916cce122ebc21cdd851db78fdb5545c55c
bkouhen <b.kouhen@gmail.com>
2017-03-19 02:02:34 +0100

    Working Http Service (Express -> Angular)
    - Angular Component & Service
    - Database items routed to an Angular Component
    - DB Items displayed in an Angular template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
@@ -17,17 +18,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require our routes into the application.
 require('./server/routes')(app);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.use(express.static(path.join(__dirname, 'mean-app/dist')));
+
+// Route to angular App
+app.get('/app', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mean-app/dist/index.html'));
+});
+
+/* Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
-}));
+}));*/
 
 // Test Authentification
 
 sequelize
   .authenticate()
   .then(function(err) {
-    console.log('Connection has been established successfully.');
+    console.log('Connection has been established successfully with PGSQL Database.');
   })
   .catch(function (err) {
     console.log('Unable to connect to the database:', err);
